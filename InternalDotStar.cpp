a30dce802e34f305f062a523495fab30398913ed
spolsky <spolsky@Jareds-iMac-3.home>
2018-11-29 16:37:40 -0500

    so yeah that was way too bright

--- InternalDotStar.cpp
+++ InternalDotStar.cpp
@@ -12,7 +12,7 @@ void InternalDotStar::setup()
 void InternalDotStar::flash( byte r, byte g, byte b )
 {
     strip.setPixelColor(0, strip.Color(r,g,b));
-    strip.setBrightness(255);
+    strip.setBrightness(96);
     strip.show();
     delay(75);
     strip.setBrightness(0);
